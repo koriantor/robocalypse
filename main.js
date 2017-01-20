8ae54175e76045f16bf8f18b572a94337116483e
Caleb George <koriantor@gmail.com>
2017-01-20 13:22:43 -0500

    Project init

---
+++ main.js
@@ -0,0 +1,21 @@
+var ClientOAuth2 = require('client-oauth2');
+
+var githubAuth = new ClientOAuth2({
+  clientId: 'abc',
+  clientSecret: '123',
+  accessTokenUri: 'https://github.com/login/oauth/access_token',
+  authorizationUri: 'https://github.com/login/oauth/authorize',
+  redirectUri: 'http://example.com/auth/github/callback',
+  scopes: ['notifications', 'gist']
+})
+
+// Install Browserify
+npm install -g browserify
+// Install client-oauth2
+npm install client-oauth2 --save
+// Convert oauth objects to 
+
+
+// TODO: Set up oauth for all roberts
+// TODO: Write tweet functions
+// TODO: Hook HTML UI to core functiosn
---
+++ readme.txt
@@ -0,0 +1,24 @@
+TWITTER TODOS:
+
+Extension Core:
+	-Icons (https://developer.chrome.com/extensions/manifest/icons)
+	-Interface for extension options
+
+Write HTML for UI
+	-Bobby Icon
+	-DDL
+	-Write JS hook for tweet button to TweetAs(bobby)
+		-Read from value from DDL
+	-Change Bobby Icon according to DDL
+	
+Write Core functions
+	-TweetAs(bobby, tweetContent)
+	-TweetResponseAs(bobby, tweetResponseContent, tweetGiven)
+	
+Nice to have in future:
+	-Easy Enable/Disable (maybe keyboard shortcut?)
+	-Options interface for advanced features
+	-Interface for scripted tweets
+	
+	
+	
