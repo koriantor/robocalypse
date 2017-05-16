a44add814bedd9126661d9fdc473e626587e3f5d
Caleb George <koriantor@gmail.com>
2017-05-16 16:41:34 -0400

    Massive Scope Reduction.
    Lesson Learned.
    
    ...probably.

---
+++ app.js
@@ -0,0 +1,12 @@
+var onTweet = function(parent) {
+    // Get from URL
+        // Call Proper Tweet fucntion
+    // Read from tweetbox
+        // Validation
+    // read from Bobdown
+    // Send info to background html
+};
+
+// TODO: Write tweet counter
+// TODO: Write authentication
+// TODO: Load JSON data
